f3c172cabedc2bcfd7e8a0f0b2bd206c368634e9
Charles Ewert <cewert@gmail.com>
2023-05-18 12:16:54 -0400

    update build-scripts with new ropm script

---
+++ build-scripts/maestro-ropm-hook.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable github/array-foreach */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fs = require('fs-extra');
+const path = require('path');
+let componentsDir = path.join(__dirname, '..', 'src', 'components', 'roku_modules');
+parseFolder(componentsDir);
+let sourceDir = path.join(__dirname, '..', 'src', 'source', 'roku_modules');
+parseFolder(sourceDir);
+function parseFolder(sourceDir) {
+    try {
+        fs.readdirSync(sourceDir).forEach(file => {
+            let filePath = path.join(sourceDir, file);
+            let fileStats = fs.statSync(filePath);
+            if (fileStats.isDirectory()) {
+                parseFolder(filePath);
+            }
+            else if (filePath.endsWith('.xml')) {
+                let text = fs.readFileSync(filePath, 'utf8');
+                let r = /\/roku_modules\/undefined\/bslib\.brs/gim;
+                text = text.replace(r, '/roku_modules/rokucommunity_bslib/bslib.brs');
+                r = /\/roku_modules\/bslib\/bslib\.brs/gim;
+                text = text.replace(r, '/roku_modules/rokucommunity_bslib/bslib.brs');
+                r = /\/roku_modules\/undefined/gim;
+                text = text.replace(r, '/roku_modules/maestro');
+                fs.writeFileSync(filePath, text);
+                // console.log('fixed', filePath);
+            }
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+//# sourceMappingURL=maestro-ropm-hook.js.map
