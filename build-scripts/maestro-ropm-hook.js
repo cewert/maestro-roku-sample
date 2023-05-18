6c52465be5c6ccdfcb57211724f52033b19f9589
Charles Ewert <cewert@gmail.com>
2023-05-18 16:39:21 -0400

    validate bslib path before replacing

--- build-scripts/maestro-ropm-hook.js
+++ build-scripts/maestro-ropm-hook.js
@@ -4,6 +4,11 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const fs = require('fs-extra');
 const path = require('path');
+let bslibPkgPath = '/roku_modules/rokucommunity_bslib/bslib.brs';
+const bslibFound = fs.existsSync('src/source' + bslibPkgPath);
+if (!bslibFound) {
+    throw new Error(`Could not locate bslib @ 'src/source' + ${bslibPkgPath}`);
+}
 let componentsDir = path.join(__dirname, '..', 'src', 'components', 'roku_modules');
 parseFolder(componentsDir);
 let sourceDir = path.join(__dirname, '..', 'src', 'source', 'roku_modules');
@@ -19,9 +24,9 @@ function parseFolder(sourceDir) {
             else if (filePath.endsWith('.xml')) {
                 let text = fs.readFileSync(filePath, 'utf8');
                 let r = /\/roku_modules\/undefined\/bslib\.brs/gim;
-                text = text.replace(r, '/roku_modules/rokucommunity_bslib/bslib.brs');
+                text = text.replace(r, bslibPkgPath);
                 r = /\/roku_modules\/bslib\/bslib\.brs/gim;
-                text = text.replace(r, '/roku_modules/rokucommunity_bslib/bslib.brs');
+                text = text.replace(r, bslibPkgPath);
                 r = /\/roku_modules\/undefined/gim;
                 text = text.replace(r, '/roku_modules/maestro');
                 fs.writeFileSync(filePath, text);
--- scripts/maestro-ropm-hook.js
+++ scripts/maestro-ropm-hook.js
@@ -1,10 +1,15 @@
-
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable github/array-foreach */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
 const fs = require('fs-extra');
 const path = require('path');
+
+let bslibPkgPath = '/roku_modules/rokucommunity_bslib/bslib.brs';
+const bslibFound = fs.existsSync('src/source' + bslibPkgPath);
+if (!bslibFound) {
+    throw new Error(`Could not locate bslib @ 'src/source' + ${bslibPkgPath}`);
+}
 
 let componentsDir = path.join(__dirname, '..', 'src', 'components', 'roku_modules');
 
@@ -23,9 +28,9 @@ function parseFolder(sourceDir) {
             } else if (filePath.endsWith('.xml')) {
                 let text = fs.readFileSync(filePath, 'utf8');
                 let r = /\/roku_modules\/undefined\/bslib\.brs/gim;
-                text = text.replace(r, '/roku_modules/rokucommunity_bslib/bslib.brs');
+                text = text.replace(r, bslibPkgPath);
                 r = /\/roku_modules\/bslib\/bslib\.brs/gim;
-                text = text.replace(r, '/roku_modules/rokucommunity_bslib/bslib.brs');
+                text = text.replace(r, bslibPkgPath);
                 r = /\/roku_modules\/undefined/gim;
                 text = text.replace(r, '/roku_modules/maestro');
                 fs.writeFileSync(filePath, text);
